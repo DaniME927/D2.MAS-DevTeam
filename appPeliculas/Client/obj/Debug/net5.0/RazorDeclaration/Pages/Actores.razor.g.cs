91b903403220f20b0a43035e1cca7fd66918d96a
sanchezd90 <sanchezd90@gmail.com>
2021-09-18 16:06:00 -0300

    Agrego campos a actores
    Agrego nombre, fecha de nacimiento y biografía

--- appPeliculas/Client/obj/Debug/net5.0/RazorDeclaration/Pages/Actores.razor.g.cs
+++ appPeliculas/Client/obj/Debug/net5.0/RazorDeclaration/Pages/Actores.razor.g.cs
@@ -13,70 +13,70 @@ namespace appPeliculas.Client.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "/Users/danielsanchez/Documents/MisionTIC/ProysCicloIII/appPeliculas/Client/_Imports.razor"
+#line 1 "/Users/danielsanchez/Documents/GitHub/D2.MAS-DevTeam/appPeliculas/Client/_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/Users/danielsanchez/Documents/MisionTIC/ProysCicloIII/appPeliculas/Client/_Imports.razor"
+#line 2 "/Users/danielsanchez/Documents/GitHub/D2.MAS-DevTeam/appPeliculas/Client/_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/Users/danielsanchez/Documents/MisionTIC/ProysCicloIII/appPeliculas/Client/_Imports.razor"
+#line 3 "/Users/danielsanchez/Documents/GitHub/D2.MAS-DevTeam/appPeliculas/Client/_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/Users/danielsanchez/Documents/MisionTIC/ProysCicloIII/appPeliculas/Client/_Imports.razor"
+#line 4 "/Users/danielsanchez/Documents/GitHub/D2.MAS-DevTeam/appPeliculas/Client/_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/Users/danielsanchez/Documents/MisionTIC/ProysCicloIII/appPeliculas/Client/_Imports.razor"
+#line 5 "/Users/danielsanchez/Documents/GitHub/D2.MAS-DevTeam/appPeliculas/Client/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/Users/danielsanchez/Documents/MisionTIC/ProysCicloIII/appPeliculas/Client/_Imports.razor"
+#line 6 "/Users/danielsanchez/Documents/GitHub/D2.MAS-DevTeam/appPeliculas/Client/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "/Users/danielsanchez/Documents/MisionTIC/ProysCicloIII/appPeliculas/Client/_Imports.razor"
+#line 7 "/Users/danielsanchez/Documents/GitHub/D2.MAS-DevTeam/appPeliculas/Client/_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "/Users/danielsanchez/Documents/MisionTIC/ProysCicloIII/appPeliculas/Client/_Imports.razor"
+#line 8 "/Users/danielsanchez/Documents/GitHub/D2.MAS-DevTeam/appPeliculas/Client/_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "/Users/danielsanchez/Documents/MisionTIC/ProysCicloIII/appPeliculas/Client/_Imports.razor"
+#line 9 "/Users/danielsanchez/Documents/GitHub/D2.MAS-DevTeam/appPeliculas/Client/_Imports.razor"
 using appPeliculas.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "/Users/danielsanchez/Documents/MisionTIC/ProysCicloIII/appPeliculas/Client/_Imports.razor"
+#line 10 "/Users/danielsanchez/Documents/GitHub/D2.MAS-DevTeam/appPeliculas/Client/_Imports.razor"
 using appPeliculas.Client.Shared;
 
 #line default
