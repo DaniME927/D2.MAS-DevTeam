c89e3a5bc633ba7687e09fc2b9e3d8d356431d18
Sandr3s <sandreslan24@gmail.com>
2021-09-23 18:58:16 -0500

    Actualización del código 23/09/2021

--- pruebaPelicula/Client/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ pruebaPelicula/Client/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,91 +13,105 @@ namespace pruebaPelicula.Client.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "/Users/danielsanchez/Downloads/pruebaPelicula/Client/_Imports.razor"
+#line 1 "C:\Users\sandr\Documents\GitHub\D2.MAS-DevTeam\pruebaPelicula\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/Users/danielsanchez/Downloads/pruebaPelicula/Client/_Imports.razor"
+#line 2 "C:\Users\sandr\Documents\GitHub\D2.MAS-DevTeam\pruebaPelicula\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/Users/danielsanchez/Downloads/pruebaPelicula/Client/_Imports.razor"
+#line 3 "C:\Users\sandr\Documents\GitHub\D2.MAS-DevTeam\pruebaPelicula\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/Users/danielsanchez/Downloads/pruebaPelicula/Client/_Imports.razor"
+#line 4 "C:\Users\sandr\Documents\GitHub\D2.MAS-DevTeam\pruebaPelicula\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/Users/danielsanchez/Downloads/pruebaPelicula/Client/_Imports.razor"
+#line 5 "C:\Users\sandr\Documents\GitHub\D2.MAS-DevTeam\pruebaPelicula\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/Users/danielsanchez/Downloads/pruebaPelicula/Client/_Imports.razor"
+#line 6 "C:\Users\sandr\Documents\GitHub\D2.MAS-DevTeam\pruebaPelicula\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "/Users/danielsanchez/Downloads/pruebaPelicula/Client/_Imports.razor"
+#line 7 "C:\Users\sandr\Documents\GitHub\D2.MAS-DevTeam\pruebaPelicula\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "/Users/danielsanchez/Downloads/pruebaPelicula/Client/_Imports.razor"
+#line 8 "C:\Users\sandr\Documents\GitHub\D2.MAS-DevTeam\pruebaPelicula\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "/Users/danielsanchez/Downloads/pruebaPelicula/Client/_Imports.razor"
+#line 9 "C:\Users\sandr\Documents\GitHub\D2.MAS-DevTeam\pruebaPelicula\Client\_Imports.razor"
 using pruebaPelicula;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "/Users/danielsanchez/Downloads/pruebaPelicula/Client/_Imports.razor"
+#line 10 "C:\Users\sandr\Documents\GitHub\D2.MAS-DevTeam\pruebaPelicula\Client\_Imports.razor"
 using pruebaPelicula.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "/Users/danielsanchez/Downloads/pruebaPelicula/Client/_Imports.razor"
+#line 11 "C:\Users\sandr\Documents\GitHub\D2.MAS-DevTeam\pruebaPelicula\Client\_Imports.razor"
 using pruebaPelicula.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "/Users/danielsanchez/Downloads/pruebaPelicula/Client/_Imports.razor"
+#line 12 "C:\Users\sandr\Documents\GitHub\D2.MAS-DevTeam\pruebaPelicula\Client\_Imports.razor"
+using pruebaPelicula.Client.Components;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 13 "C:\Users\sandr\Documents\GitHub\D2.MAS-DevTeam\pruebaPelicula\Client\_Imports.razor"
+using pruebaPelicula.Shared.Entity;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 14 "C:\Users\sandr\Documents\GitHub\D2.MAS-DevTeam\pruebaPelicula\Client\_Imports.razor"
 using Blazored.Modal;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "/Users/danielsanchez/Downloads/pruebaPelicula/Client/_Imports.razor"
+#line 15 "C:\Users\sandr\Documents\GitHub\D2.MAS-DevTeam\pruebaPelicula\Client\_Imports.razor"
 using Blazored.Modal.Services;
 
 #line default
@@ -111,7 +125,7 @@ using Blazored.Modal.Services;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 28 "/Users/danielsanchez/Downloads/pruebaPelicula/Client/Shared/NavMenu.razor"
+#line 33 "C:\Users\sandr\Documents\GitHub\D2.MAS-DevTeam\pruebaPelicula\Client\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
